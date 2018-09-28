f0cc783cd54ea1b2f576ec4a80258f92ce996c6b
Xiaoyan Zou <xiaoyan.zou@epfl.ch>
2018-09-28 12:36:43 +0200

    Add printing

--- 01/LinkedList.java
+++ 01/LinkedList.java
@@ -10,4 +10,14 @@ class LinkedList{
             data = d;
         }
     }
+public void printList(){  
+    Node n = head; 
+    while (n != null){ 
+        System.out.print(n.data+" "); 
+        n = n.next;
+    }
+}  
+
+
+
 }
